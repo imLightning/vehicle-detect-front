dbe1063e568d6b80420c5da809e1f96a5ce1f56a
imLightning <2744002576@qq.com>
2024-06-25 12:09:11 +0800

    新增录像管理

---
+++ src/views/manager/fileAdmin.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <el-table :data="data.file" style="width: 100%">
+            <el-table-column prop="id" label="序号" width="120" />
+            <el-table-column prop="filename" label="录像"/>
+            <el-table-column prop="state" label="状态" width="120" />
+            <el-table-column prop="time" label="时间" width="120" />
+            <el-table-column fixed="right" label="操作" min-width="120">
+            <template #default>
+                <el-button link type="primary" size="small">检测</el-button>
+            </template>
+            </el-table-column>
+        </el-table>
+    </div>
+</template>
+
+<script setup>
+import { getfile } from '@/api/file'
+import { reactive } from 'vue'
+
+const data = reactive({
+    file: []
+})
+
+const load = () => {
+    getfile().then(res => {
+        data.file = res.data
+    })
+}
+
+load()
+</script>
