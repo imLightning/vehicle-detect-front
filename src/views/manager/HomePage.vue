ea3192bad8ec005e82a66173908759d159b855f6
ygs <ygs7280@outlook.com>
2024-06-26 19:56:49 +0800

    首页和详情页可视化弹窗

--- src/views/manager/HomePage.vue
+++ src/views/manager/HomePage.vue
@@ -1,98 +1,22 @@
 <template>
   <div class="bg">
     <h1 class="header">欢迎使用智慧交通车速检测系统</h1>
-    <e-row class="row">
-      <e-col :span="12" class="char-zx">
-        <div class="chart-container" ref="chartRef"></div>
-      </e-col>
-      <e-col :span="6" class="btn-div">
-        <div class="btn">
-          <el-button class="button" type="primary" @click="toFilePage()">
-            进入工作台<el-icon><ArrowRightBold /></el-icon>
-          </el-button>
-        </div>
-      </e-col>
-    </e-row>
+    <div class="btn">
+      <el-button class="button" type="primary" @click="toFilePage()">
+        进入工作台<el-icon><ArrowRightBold /></el-icon>
+      </el-button>
+    </div>
   </div>
 </template>
  
 <script setup>
-//引入echarts
-import * as echarts from 'echarts';
-import { ref, onMounted, onUnmounted } from 'vue';
+// import { ref, onMounted, onUnmounted } from 'vue';
 import { ArrowRightBold } from '@element-plus/icons-vue';
 import { useRouter } from "vue-router";
 
 
 const $router = useRouter();
 
-const chartRef = ref(null)
-let chartInstance = null
-
-onMounted(() => {
-  chartInstance = echarts.init(chartRef.value)
-
-  const data = [
-    { name: '<20', value: 10 },
-    { name: '20~30', value: 20 },
-    { name: '30~40', value: 15 },
-    { name: '40~50', value: 30 },
-    { name: '50~60', value: 50 },
-    { name: '60~70', value: 40 },
-    { name: '70~80', value: 12 },
-    { name: '>80', value: 10 }
-  ]
-
-  // 配置项
-  const options = {
-    title: {
-      text: '车辆速度数据',
-      subtext: '车辆速度分布折线图',
-      x: 'center'
-    },
-    xAxis: {
-      name: '速度(km/h)',
-      type: 'category',
-      data: data.map((item) => item.name),
-      axisTick: {
-        show: false
-      },
-
-    },
-    yAxis: {
-      name: '车辆数(辆)',
-      type: 'value'
-    },
-    series: [
-      {
-        data: data.map((item) => item.value),
-        type: 'line', 
-        smooth: true,
-        emphasis: {
-          itemStyle: {
-            borderWidth: 6,
-            shadowBlur: 10, 
-            shadowColor: '#ccc'
-          }
-        }
-      }
-    ],
-    tooltip: {
-      trigger: 'item',
-      formatter: '{b}: {c}'
-    }
-  }
-
-  // 设置图表配置项
-  chartInstance.setOption(options)
-})
-
-onUnmounted(() => {
-  if (chartInstance) {
-    chartInstance.dispose()
-    chartInstance = null
-  }
-})
 
 const toFilePage = () => {
   $router.push({ path: "/file" })
@@ -110,39 +34,24 @@ const toFilePage = () => {
 }
 
 .header {
+  color: #ffffff;
+  text-shadow: 3px 2px 5px #999;
   text-align: center;
-  font-size: 50px;
+  font-size: 70px;
   margin: 0 0;
-}
-
-.char-zx{
-  width: 600px;
-  height: 400px;
-  border: 1px solid #ccc;
-  box-shadow: 0 0 5px 1px #999;
-  margin: 30px 10px;
-}
-
-.chart-container {
-  width: 600px;
-  height: 400px;
-  margin: 40px 10px;
-  border-radius: 30px;
-  background-color: rgb(185, 225, 255);
-  box-shadow: 0 0 5px 1px rgb(185, 225, 255);
+  margin-top: 50px;
 }
 
 
-.btn-div {
-  width: auto;
-  height: 500px;
-}
 
 .btn {
   width: 400px;
   height: 150px;
   padding-top: 200px;
-  padding-left: 150px;
+  padding-bottom: 320px;
+  text-align: center;
+  display: block;
+  margin: 0 auto;
 }
 
 .button {
@@ -150,5 +59,11 @@ const toFilePage = () => {
   width: 100%;
   border-radius: 30px;
   font-size: 50px;
+  box-shadow: 0 0 20px 10px #67b5ff;
 }
+
+el-button {
+  color: rgb(255, 171, 103)
+}
+
 </style>
--- src/views/manager/ResultAdmin.vue
+++ src/views/manager/ResultAdmin.vue
@@ -14,20 +14,55 @@
             </el-table-column>
         </el-table>
     </div>
+
+    <el-dialog v-model="infoDialog" title="车辆信息数据" width="850">
+        <el-button-group>
+            <el-button type="primary" @click="showZX">查看折线图</el-button>
+            <el-button type="primary" @click="showB">
+            查看饼状图
+            </el-button>
+        </el-button-group>
+        <el-dialog v-model="innerDialog" title="车辆速度占比" width="800" append-to-body>
+            <div class="chartB" id="chartB"></div>
+            <div>
+                总车次：{{ cnt }}辆
+            </div>
+            <div>
+                超速车次：{{ speeding }}辆
+            </div>
+        </el-dialog>
+
+        <div class="chartZX" id="chartZX"></div>
+        <template #footer>
+            <div class="dialog-footer">
+                <el-button @click="openInner = false">关闭</el-button>
+                <el-button type="primary" @click="openInner()">
+                查看饼状图
+                </el-button>
+            </div>
+        </template>
+
+        
+    </el-dialog>
 </template>
 
 <script setup>
+//引入echarts
+import * as echarts from 'echarts';
 import { getRes, delRes } from '@/api/result'
-import { reactive } from 'vue'
+import { ref, reactive } from 'vue'
 import { secondaryConfirm } from '@/utils/popups'
 
 const data = reactive({
-    file: []
+    file: [],
+    speedInfo: {},
+    cntInfo: [0, 0, 0, 0, 0, 0, 0]
 })
 
 const load = () => {
     getRes().then(res => {
         data.file = res.data
+        cnt.value = 0
     }).catch(() => {})
 }
 
@@ -46,8 +81,164 @@ const handle_del = (id, filename) => {
     })
 }
 
+const cnt = ref(0)
+
+// const chartCnt = reactive([0, 0, 0, 0, 0, 0, 0])
+
+const infoDialog = ref(false)
+const echartsX = ['<80', '80~90', '90~100', '100~110', '110~120', '120~130', '>130']
+
 const showInfo = (info) => {
-    console.log(info);
+    cnt.value = Object.keys(data.speedInfo).length
+    data.speedInfo = JSON.parse(info)
+    infoDialog.value = true;
+    count(data.speedInfo)
+    setTimeout(() => {
+        let infoChart = echarts.init(document.getElementById("chartZX"))
+        infoChart.setOption(optionZX)
+    }, 1) 
 }
 
+const innerDialog = ref(false)
+
+const openInner = () => {
+    cnt.value = Object.keys(data.speedInfo).length
+    innerDialog.value = true;
+    count(data.speedInfo)
+    setTimeout(() => {
+        let infoChart = echarts.init(document.getElementById("chartB"))
+        infoChart.setOption(optionB)
+    })
+}
+
+const showZX = () => {
+    cnt.value = Object.keys(data.speedInfo).length
+    infoDialog.value = true;
+    count(data.speedInfo)
+    setTimeout(() => {
+        let infoChart = echarts.init(document.getElementById("chartZX"))
+        infoChart.setOption(optionZX)
+    }, 1) 
+}
+
+const showB = () => {
+
+}
+
+
+const optionZX = reactive({
+    title: {
+        text: '车辆速度数据',
+        subtext: '车辆速度分布折线图',
+        x: 'center'
+    },
+    xAxis: {
+        name: '速度(km/h)',
+        type: 'category',
+        data: echartsX,
+        axisTick: {
+            show: false
+        },
+    },
+    yAxis: {
+        name: '车辆数(辆)',
+        type: 'value'
+    },
+    series: [{
+        data: data.cntInfo,
+        type: 'line',
+        smooth: true,
+        emphasis: {
+            itemStyle: {
+                borderWidth: 6,
+                shadowBlur: 10,
+                shadowColor: '#ccc'
+            }
+        }
+    }],
+    tooltip: {
+        trigger: 'item',
+        formatter: '{b}: {c}'
+    }
+})
+
+
+const count = (infoData) => {
+    for (let key in infoData) {
+        if (infoData[key] <= 80) {
+            data.cntInfo[0]++;
+        } else if (infoData[key] >= 80 && infoData[key] < 90) {
+            data.cntInfo[1]++;
+        } else if (infoData[key] >= 90 && infoData[key] < 100) {
+            data.cntInfo[2]++;
+        } else if (infoData[key] >= 100 && infoData[key] < 110) {
+            data.cntInfo[3]++;
+        } else if (infoData[key] >= 110 && infoData[key] < 120) {
+            data.cntInfo[4]++;
+        } else if (infoData[key] >= 120 && infoData[key] < 130) {
+            data.cntInfo[5]++;
+        } else if (infoData[key] >= 130) {
+            data.cntInfo[6]++;
+        }
+    }
+}
+
+
+const optionB = {
+    tooltip: {
+    trigger: 'item'
+  },
+  legend: {
+    top: '5%',
+    left: 'center'
+  },
+  series: [
+    {
+      name: 'Access From',
+      type: 'pie',
+      radius: ['40%', '70%'],
+      avoidLabelOverlap: false,
+      itemStyle: {
+        borderRadius: 10,
+        borderColor: '#fff',
+        borderWidth: 2
+      },
+      label: {
+        show: false,
+        position: 'center'
+      },
+      emphasis: {
+        label: {
+          show: true,
+          fontSize: 30,
+          fontWeight: 'bold'
+        }
+      },
+      labelLine: {
+        show: false
+      },
+      data: [
+        { value: data.cntInfo[0], name: echartsX[0] },
+        { value: data.cntInfo[1], name: echartsX[1] },
+        { value: data.cntInfo[2], name: echartsX[2] },
+        { value: data.cntInfo[3], name: echartsX[3] },
+        { value: data.cntInfo[4], name: echartsX[4] }
+      ]
+    }
+  ]
+}
 </script>
+
+<style>
+.chartZX {
+    width: 800px;
+    height: 400px;
+    padding: 10px;
+}
+
+.chartB {
+    width: 800px;
+    height: 400px;
+    padding: 10px;
+}
+</style>
